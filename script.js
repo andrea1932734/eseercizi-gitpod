5e3b56b0142aeda7b78f545d8b41da716739502a
andrea1932734 <andreacarovelli@gmail.com>
2021-11-19 11:37:37 +0000

    vers 1

---
+++ script.js
@@ -0,0 +1,26 @@
+let i=0;
+function funz1() {
+    i++;
+    document.getElementById("a").innerHTML = "Pvvvvvontone"
+    document.getElementById("b").innerHTML = "Disegna"
+    if (i==2) {
+        document.getElementById("a").innerHTML = "Disegnone"
+        document.getElementById("b").innerHTML = "Ricomincia"
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, 200,200);
+       
+    }
+
+    if (i==3) {
+        document.getElementById("a").innerHTML = "ciaone"
+        document.getElementById("b").innerHTML = "premimi"
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "orange";
+        ctx.fillRect(0, 0, 200,200);
+        i=0;
+    }
+    
+}
